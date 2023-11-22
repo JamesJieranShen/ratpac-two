9fda0f0605e9a710d3ebcdb1ff739b075e5fb8d3
James Shen <jamesjieranshen@gmail.com>
2023-11-22 12:20:18 -0500

    dump ratdb to json

--- src/db/src/DB.cc
+++ src/db/src/DB.cc
@@ -467,4 +467,17 @@ std::vector<DBTable *> DB::ReadRATDBFile(const std::string &filename) {
   return contents;
 }
 
+void DB::DumpContentsToJson(std::ostream &stream) {
+  json::Writer writer(stream);
+  stream << "[\n";
+  const DBTableSet &tables = GetAllTables();
+  bool first = true;
+  for (auto table : tables) {
+    if (!first) stream << ",\n";
+    first = false;
+    writer.putValue(table.second->GetCompleteJSON(), "");
+  }
+  stream << "\n]\n";
+}
+
 }  // namespace RAT
